25c4d059b89c446d17d5b9294c8f993b577f0902
Choaib ELMADI <choaib3elmadi@gmail.com>
2024-02-22 14:17:36 +0100

    Show a specific job offer details

--- condidat/urls.py
+++ condidat/urls.py
@@ -4,8 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="home"),
     path("verif", views.upload_file, name="redirect"),
-    path(
-        "condidature", views.upload, name="condidation"
-    ),  # route vers details off +telechr cv
-    path("search-job-offers", views.search_job_offers, name="search-job-offers"),
+    path("condidature", views.upload, name="condidation"),
+    path("search-job-offers/", views.search_job_offers, name="search-job-offers"),
+    path("<str:slug>", views.offer_details, name="offer-details"),
 ]
