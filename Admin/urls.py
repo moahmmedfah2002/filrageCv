d7bd1526594b84301bdb38b844661ac634d10d0a
FAHLAOUI MOHAMMED <fahlaouimohammed@gmail.com>
2024-02-22 14:39:32 +0100

    design

--- Admin/urls.py
+++ Admin/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path("",views.index,name="test"),
-    path("candidate/",views.candidate, name="candidate")
+    path("/candidate",views.candidate, name="candidate")
 ]
