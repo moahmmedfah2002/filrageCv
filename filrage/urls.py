f6e8af2756c9ba45e96ced3d104ca8a54d41c1f0
Choaib ELMADI <choaib3elmadi@gmail.com>
2024-02-22 12:03:26 +0100

    Remove the urls warnings (Remove the '/')

--- filrage/urls.py
+++ filrage/urls.py
@@ -1,11 +1,10 @@
-
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 
 urlpatterns = [
-    path("Login",include("Login.urls")),
-    path("Admin",include("Admin.urls")),
-    path("condidat",include("condidat.urls")),
-    path("recruteur",include("recruteur.urls")),
-
+    path("admin/", admin.site.urls),
+    path("Login/", include("Login.urls")),
+    path("Admin/", include("Admin.urls")),
+    path("condidat/", include("condidat.urls")),
+    path("recruteur/", include("recruteur.urls")),
 ]
